42db3f7f41f5936a7de42629b852f75391519fc7
Will-S <willschrick@gmail.com>
2024-12-08 05:37:38 -0800

    books working

--- routes/books.js
+++ routes/books.js
@@ -6,16 +6,21 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 router.get("/", async (req, res) => {
+  console.log("GET /books route hit");
 //router.get("/books", async (req, res) => {
   const books =  await prisma.libro.findMany({
     select: {
       ISBN: true,
       Titulo: true,
-      Autor: true,
       Precio: true,
     },
   });
-  res.json(books); // Send the data as a JSON response
+  console.log("Books fetched successfully:", books);
+  res.json({
+    message: "Books fetched successfully",
+    books: books,
+  });
+  //res.json(books); // Send the data as a JSON response
 });
 
 // Test route for "/books/test"
